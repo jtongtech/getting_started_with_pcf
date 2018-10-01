a2c9df00f27a0959874d89851acbe66dfae8100f
Tong <jeremy.tong@capgemini.com>
2018-10-01 09:55:26 -0400

    initial commit

---
+++ src/main/java/com/example/demo/JtDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtDemoApplication.class, args);
+	}
+}
